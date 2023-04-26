4db9f116ab69396eb822cc8bacf3b16029d6e0c0
Marianasuyane <marisuyane26@gmail.com>
2023-04-26 11:08:26 -0300

    primeiro commit

--- src/public/index.php
+++ src/public/index.php
@@ -1,3 +1,31 @@
 <?php
+$saldo = 10;
+if($saldo <100){
+    $deposito = 1000;
+    $saldo += $deposito;
 
-phpinfo();
+}
+
+echo $saldo;
+//$nome = 'Maria';
+//$idade = 30;
+
+//echo 'a: [' . 73 . ']<BR />';
+//echo 'b: [' . 'Oi' . ']<BR />';
+//echo 'c: [' . (False) . ']<BR />';
+//echo 'd: [' . ($nome) . ']<BR />';
+//echo 'e: [' . ($idade) . ']<BR />';
+
+/*
+//phpinfo();
+$contador = 1;
+$mensagem = "Olá";
+$versao = array ('1.1','2','3.4');
+//print_r($versao);
+$primeiro_nome = "Maria";
+echo $primeiro_nome;
+echo '<br />';
+$ultimo_nome = "Lourdes";
+echo $ultimo_nome;
+*/
+?>
